307fecf66bc4b76b4d062c8728bd821dbeed9ff0
tushrsm <tusharmandavkar4@gmail.com>
2016-11-10 20:41:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles ={
-tushar01 :{
+`tushar01 :{
 title:'tushar-01',
 heading: 'hello guys',
 date: '08/11/2016',
@@ -20,7 +20,7 @@ content:     `<div>
             </div>
         </div>`
 },
-tushar02:{
+`tushar02:{
 title:'tushar-02',
 heading: 'hello guys',
 date: '10/11/2016',
@@ -34,7 +34,7 @@ content:     `<div>
             </div>
         </div>`
 },
-tushar03:{
+`tushar03:{
 title:'tushar-01',
 heading: 'hello guys',
 date: '11/11/2016',
