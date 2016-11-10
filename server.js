3db455231c8e1e00dac2ec5f003cd4edb7ef278d
tushrsm <tusharmandavkar4@gmail.com>
2016-11-10 20:19:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,9 @@ function createTemplate (data){
     var heading=data.heading;
     var date=data.date;
     var content=data.content;
-var htmltemplate=`
+    
+    
+var htmlTemplate=`
 <html>
     <head>
         <title>
