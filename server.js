743fec85558ff68542b62a304f739e557bc00160
tushrsm <tusharmandavkar4@gmail.com>
2016-11-12 12:41:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,104 +5,15 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles ={
-    `article-one :{
-title:'article-one',
-heading: 'hello guys',
-date: '08/11/2016',
-content:     `<div>
-                      <p> 
-                    hi im Tushar Mandavkar,i like to be anew app devloper. 
-                    </p>
-            </div>
-            <div>
-                are  you enjoy????
-            </div>
-        </div>`
-},
-    `article-two :{
-title:'article-two',
-heading: 'hello guys',
-date: '10/11/2016',
-content:     `<div>
-                      <p> 
-                these for 2nd    hi im Tushar Mandavkar,i like to be anew app devloper. 
-                    </p>
-            </div>
-            <div>
-                are  you enjoy????
-            </div>
-        </div>`
-},
 
-    article-three :{
-title:'article-three',
-heading: 'hello guys',
-date: '11/11/2016',
-content:     `<div>
-                      <p> 
-                these for 2nd    hi im Tushar Mandavkar,i like to be anew app devloper. 
-                    </p>
-            </div>
-            <div>
-                are  you enjoy????
-            </div>
-        </div>`
-}
-};
-
-function createTemplate (data){
-    var title=data.title;
-    var heading=data.heading;
-    var date=data.date;
-    var content=data.content;
-    
-    
-var htmlTemplate=`
-<html>
-    <head>
-        <title>
-            ${title}
-        </title>
-         <link href="/ui/style.css" rel="stylesheet" />
-        <style>
-   
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div> 
-                <a href='/'>Home</a> 
-            </div>
-            <div>
-               ${heading}
-            </div>
-            <div>
-                ${date}
-            </div>
-            <hr/>
-            <div>
-           ${content}
-            </div>
-        </div>
-    </body>
-</html>
-`;
-return htmlTemplate;
-}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
-
-app.get('/:articleName', function (req, res) {
- var articleName=req.pqrqms.articlename;
-res.send(createTemplate(articles[articleName]));
+app.get('/tushar-01', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'tushar-01.html'));
 });
-
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
